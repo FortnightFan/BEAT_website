e0feeef0bdd0ec13588170d2ebc503a5a90098c4
FortnightFan <141198301+FortnightFan@users.noreply.github.com>
2024-03-10 21:37:32 -0700

    Updated logo, added drawer

--- src/pages/components/NavigationBar.jsx
+++ src/pages/components/NavigationBar.jsx
@@ -1,24 +1,65 @@
-import AccessibilityOutlinedIcon from '@mui/icons-material/AccessibilityOutlined';
+import React from 'react';
+import './Styles.css';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import Logo from './BEAT_Logo_Black.png';
-import './Styles.css';
 
 const NavigationBar = () => {
+
+  const [open, setOpen] = React.useState(false);
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 450 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <ListItemButton>
+          <ListItemText primary='hello' />
+        </ListItemButton>
+      </List>
+
+      <Divider />
+
+      <List>
+        <ListItemButton>
+          <ListItemText primary='First' />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemText primary='Second' />
+        </ListItemButton>
+
+        <ListItemButton>
+          <ListItemText primary='Third' />
+        </ListItemButton>
+
+      </List>
+    </Box>
+  );
+
+
   return (
-    <div className="navbar">
-      <div>
-        {/* <img src={Logo} alt="B.E.A.T Logo" className="site-logo" /> */}
-        <IconButton aria-label="delete" size="small">
-          <AccessibilityOutlinedIcon fontSize='large'/>
+    <div>
+      <Box bgcolor={'maroon'}>
+        <IconButton onClick={toggleDrawer(true)} aria-label="delete" size="small">
+          <img src={Logo} alt="B.E.A.T Logo" className="site-logo" />
         </IconButton>
 
-        <Button variant="contained" sx={{ ml: 1, backgroundColor: 'orange' }}>About</Button>
-        <Button variant="contained" sx={{ ml: 1 }}>Register</Button>
-        <Button variant="contained" sx={{ ml: 1 }}>Login</Button>
-      </div>
+        <Drawer sx={{color: "red",}} open={open} onClose={toggleDrawer(false)}>
+          {DrawerList}
+        </Drawer>
       
+        <Button variant="contained" sx={{ ml: 3, backgroundColor: 'orange' }}>About</Button>
+        <Button variant="contained" sx={{ ml: 3 }}>Register</Button>
+        <Button variant="contained" sx={{ ml: 3 }}>Login</Button>
+      </Box>
     </div>
 
   );

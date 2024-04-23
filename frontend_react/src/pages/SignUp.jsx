7ff7ed1d50c61222a3afbf7ae9fbab35bc0fe690
Antonio Balestrino <antonio.balestrino2@gmail.com>
2024-04-23 16:40:16 -0700

    Update SignUp.jsx
    fixed the fetch link to work correctly

--- frontend_react/src/pages/SignUp.jsx
+++ frontend_react/src/pages/SignUp.jsx
@@ -38,7 +38,7 @@ export default function SignUp() {
     };
   
     try {
-      const response = await fetch('http://localhost:8000/accounts/register/', {
+      const response = await fetch('http://127.0.0.1:8000/accounts/register/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

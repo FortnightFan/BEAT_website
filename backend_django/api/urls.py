461bc128f42936bdcc3b094f4395d15e355e7120
FortnightFan <141198301+FortnightFan@users.noreply.github.com>
2024-04-24 07:18:58 -0700

    Added exercise filtering from backend

--- backend_django/api/urls.py
+++ backend_django/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import filter_exercises
 
 urlpatterns = [
     path("exercises/", views.ExerciseListCreate.as_view(), name="exercise-view-create"),
+    path('filter/', filter_exercises, name='filter'), 
 ]
--- backend_django/api/views.py
+++ backend_django/api/views.py
@@ -3,8 +3,17 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import Exercise
 from .serializers import ExerciseSerializer
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 class ExerciseListCreate(generics.ListCreateAPIView):
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
 
+def filter_exercises(request):
+    # data = json.loads(request.body)
+    print("hello")
+    queryset = Exercise.objects.filter(primaryMuscles='["lats"]')
+    data = list(queryset.values())
+    return JsonResponse(data, safe=False)
